ea05270d31f3eb6ddea94e0d4fba7756e739d61c
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-06 18:46:15 +0100

    Update about.php

--- includes/about.php
+++ includes/about.php
@@ -1,1 +1,32 @@
-<b>Meterpreter</b> is an advanced, dynamically extensible payload that uses in-memory DLL injection stagers and is extended over the network at runtime. It communicates over the stager socket and provides a comprehensive client-side Ruby API. It features command history, tab completion, channels, and more. Metepreter was originally written by skape for Metasploit 2.x, common extensions were merged for 3.x and is currently undergoing an overhaul for Metasploit 3.3. The server portion is implemented in plain C and is now compiled with MSVC, making it somewhat portable. The client can be written in any language but Metasploit has a full-featured Ruby client API.
+Running Metasploit Remotely
+<br><br>
+Metasploit Framework can be run as a service and used remotely. 
+<br><br>
+The main advantage of running Metasploit remotely is that you can control it with your own custom security scripts <br>
+or you can control it from anywhere in the world from any device that has a terminal and supports Ruby.</font>
+<br><br>
+This module starts msfrpcd. 
+<br><br>
+Using the MSFRPCD Utility :
+<br><br>
+
+<font style='font-family: monospace; font-weight: bold;'>
+msfrpcd -OPTIONS
+<br>
+OPTIONS:
+<br><br>
+    -P <opt>  Specify the password to access msfrpcd<br>
+    -S        Disable SSL on the RPC socket<br>
+    -U <opt>  Specify the username to access msfrpcd<br>
+    -a <opt>  Bind to this IP address (default: 0.0.0.0)<br>
+    -c        (JSON-RPC) Path to certificate (default: /root/.msf4/msf-ws-cert.pem)<br>
+    -f        Run the daemon in the foreground<br>
+    -h        Help banner<br>
+    -j        (JSON-RPC) Start JSON-RPC server<br>
+    -k        (JSON-RPC) Path to private key (default: /root/.msf4/msf-ws-key.pem)<br>
+    -n        Disable database<br>
+    -p <opt>  Bind to this port (default: 55553)<br>
+    -t <opt>  Token Timeout seconds (default: 300)<br>
+    -u <opt>  URI for Web server<br>
+    -v        (JSON-RPC) SSL enable verify (optional) client cert requests<br>
+</font>
