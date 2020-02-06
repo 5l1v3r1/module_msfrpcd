4ad511feaf74ca03501e85661b16fdcef3c12a5f
H4xl0r <31049432+H4xl0r@users.noreply.github.com>
2020-02-06 18:43:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <? 
 /*
-    Copyright (C) 2013-2014 xtr4nge [_AT_] gmail.com
+    Copyright (C) 2013-2020 xtr4nge [_AT_] gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8" />
-<title>FruityWifi</title>
+<title>FruityWifi : Msfrpcd</title>
 <script src="../js/jquery.js"></script>
 <script src="../js/jquery-ui.js"></script>
 <link rel="stylesheet" href="../css/jquery-ui.css" />
@@ -51,7 +51,7 @@ include "../../functions.php";
 
 // Checking POST & GET variables...
 if ($regex == 1) {
-	regex_standard($_POST["newdata"], "msg.php", $regex_extra);
+    regex_standard($_POST["newdata"], "msg.php", $regex_extra);
     regex_standard($_GET["logfile"], "msg.php", $regex_extra);
     regex_standard($_GET["action"], "msg.php", $regex_extra);
     regex_standard($_POST["service"], "msg.php", $regex_extra);
@@ -66,7 +66,6 @@ $service = $_POST["service"];
 // DELETE LOG
 if ($logfile != "" and $action == "delete") {
     $exec = "$bin_rm ".$mod_logs_history.$logfile.".log";
-    //exec("$bin_danger \"$exec\"", $dump); //DEPRECATED
     exec_fruitywifi($exec);
 }
 
@@ -74,7 +73,6 @@ if ($logfile != "" and $action == "delete") {
 if ($_POST["change_mode"] == "1") {
     $ss_mode = $service;
     $exec = "$bin_sed -i 's/ss_mode.*/ss_mode = \\\"".$ss_mode."\\\";/g' includes/options_config.php";
-    //exec("$bin_danger \"$exec\"", $output); //DEPRECATED
     $output = exec_fruitywifi($exec);
 }
 
@@ -83,18 +81,21 @@ if ($_POST["change_mode"] == "1") {
 <div class="rounded-top" align="left"> &nbsp; <b><?=$mod_alias?></b> </div>
 <div class="rounded-bottom">
 
-    &nbsp;&nbsp;version <?=$mod_version?><br>
+    &nbsp;version <?=$mod_version?><br>
+    <? 
+    if (file_exists($bin_msfrpcd)) { 
+        echo "&nbsp; $mod_name <font style='color:lime'>installed</font><br>";
+    } else {
+        echo "&nbsp; $mod_name <a href='includes/module_action.php?install=install_$mod_name' style='color:red'>install</a><br>";
+    } 
+    ?>
     
     <?
-    $ismoduleup = exec("ps auxww | grep $mod_name | grep -v -e 'grep $mod_name'");
+    $ismoduleup = exec($mod_isup);
     if ($ismoduleup != "") {
-        echo "&nbsp;&nbsp;$mod_alias  <font color=\"lime\"><b>enabled</b></font>.&nbsp; | <a href=\"includes/module_action.php?service=$mod_name&action=stop&page=module\"><b>stop</b></a>";
-        echo "<input type='hidden' name='action' value='stop'>";
-        echo "<input type='hidden' name='page' value='module'>";
+        echo "&nbsp; $mod_alias  <font color='lime'><b>enabled</b></font>.&nbsp; | <a href='includes/module_action.php?service=$mod_name&action=stop&page=module'><b>stop</b></a>";
     } else { 
-        echo "&nbsp;&nbsp;$mod_alias  <font color=\"red\"><b>disabled</b></font>. | <a href=\"includes/module_action.php?service=$mod_name&action=start&page=module\"><b>start</b></a>"; 
-        echo "<input type='hidden' name='action' value='start'>";
-        echo "<input type='hidden' name='page' value='module'>";
+        echo "&nbsp; $mod_alias  <font color='red'><b>disabled</b></font>. | <a href='includes/module_action.php?service=$mod_name&action=start&page=module'><b>start</b></a>"; 
     }
     ?>
 
@@ -112,19 +113,49 @@ Loading, please wait...
     <div id="result" class="module">
         <ul>
             <li><a href="#result-1">Setup</a></li>
-            <li><a href="#result-2">About</a></li>
+	    <li><a href="#result-2">Output</a></li>
+	    <li><a href="#result-3">History</a></li>
+            <li><a href="#result-4">About</a></li>
         </ul>
         <div id="result-1">
 	    <form method="POST" autocomplete="off" action="includes/save.php">
                 <div class="module-options">
                     <table>
                         <tr>
-                            <td>Host: </td>
-                            <td><input name="meterpreter_host" value="<?=$meterpreter_host?>"></td>
+                            <td>msfrpcd host: </td>
+                            <td><input name="msfrpcd_host" value="<?=$msfrpcd_host?>"></td>
                         </tr>
                         <tr>
-                            <td>Port: </td>
-                            <td><input name="meterpreter_port" value="<?=$meterpreter_port?>"></td>
+                            <td>msfrpcd port: </td>
+                            <td><input name="msfrpcd_port" value="<?=$msfrpcd_port?>"></td>
+                        </tr>
+			<tr>
+                            <td>msfrpcd user: </td>
+                            <td><input name="msfrpcd_user" value="<?=$msfrpcd_user?>"></td>
+                        </tr>
+			<tr>
+                            <td>msfrpcd pw: </td>
+                            <td><input name="msfrpcd_pw" value="<?=$msfrpcd_pw?>"></td>
+                        </tr>
+			<tr>
+                            <td>msfrpcd db: </td>
+                            <td><input name="msfrpcd_db" value="<?=$msfrpcd_db?>"></td>
+                        </tr>
+			<tr>
+                            <td>msfrpcd sslenable: </td>
+                            <td><input name="msfrpcd_sslenable" value="<?=$msfrpcd_sslenable?>"></td>
+                        </tr>
+			<tr>
+                            <td>msfrpcd sslverify: </td>
+                            <td><input name="msfrpcd_sslverify" value="<?=$msfrpcd_sslverify?>"></td>
+                        </tr>
+			<tr>
+                            <td>msfrpcd sslcertpath: </td>
+                            <td><input name="msfrpcd_sslcertpath" value="<?=$msfrpcd_sslcertpath?>"></td>
+                        </tr>
+			<tr>
+                            <td>msfrpcd sslprivkeypath: </td>
+                            <td><input name="msfrpcd_sslprivkeypath" value="<?=$msfrpcd_sslprivkeypath?>"></td>
                         </tr>
                         <tr>
                             <td></td>
@@ -139,9 +170,57 @@ Loading, please wait...
             
         </div>
 
+	<!-- LOGS -->
+
+        <div id="result-2">
+            <form id="formLogs-Refresh" name="formLogs-Refresh" method="POST" autocomplete="off" action="index.php">
+            <input type="submit" value="refresh">
+            <br><br>
+            <?
+                if ($logfile != "" and $action == "view") {
+                    $filename = $mod_logs_history.$logfile.".log";
+                } else {
+                    $filename = $mod_logs;
+                }
+            
+                $data = open_file($filename);                
+            ?>
+            <textarea id="output" class="module-content" style="font-family: courier;"><?=htmlspecialchars($data)?></textarea>
+            <input type="hidden" name="type" value="logs">
+            </form>
+            
+        </div>
+
+        <!-- END LOGS -->
+
+	 <!-- HISTORY -->
+
+        <div id="result-3" class="history">
+            <input type="submit" value="refresh">
+            <br><br>
+            
+            <?
+            $logs = glob($mod_logs_history.'*.log');
+            print_r($a);
+
+            for ($i = 0; $i < count($logs); $i++) {
+                $filename = str_replace(".log","",str_replace($mod_logs_history,"",$logs[$i]));
+                echo "<a href='?logfile=".str_replace(".log","",str_replace($mod_logs_history,"",$logs[$i]))."&action=delete&tab=2'><b>x</b></a> ";
+                echo $filename . " | ";
+                echo "<a href='?logfile=".str_replace(".log","",str_replace($mod_logs_history,"",$logs[$i]))."&action=view'><b>view</b></a>";
+                echo "<br>";
+            }
+            ?>
+            
+        </div>
+        
+        <!-- END HISTORY -->
+
+
+
         <!-- ABOUT -->
 
-        <div id="result-2" class="history">
+        <div id="result-4" class="history">
             <? include "includes/about.php"; ?>
         </div>
 
@@ -261,7 +340,16 @@ Loading, please wait...
         echo "<script>";
         echo "$( '#result' ).tabs({ active: 4 });";
         echo "</script>";
-    } 
+    } else if ($_GET["tab"] == 5) {
+        echo "<script>";
+        echo "$( '#result' ).tabs({ active: 5 });";
+        echo "</script>";
+    } else if ($_GET["tab"] == 6) {
+        echo "<script>";
+        echo "$( '#result' ).tabs({ active: 6 });";
+        echo "</script>";
+    }
+
     ?>
 
 </div>
